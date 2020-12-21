78b85f00f521ada4086465257c8d534cf9ffc373
buck-yeh <buck.yeh@gmail.com>
2020-12-21 20:55:17 +0800

    1) [New] Example cmake project to verify build after installation of parsergen & bux from AUR.

---
+++ test/archlinux/main.cpp
@@ -0,0 +1,66 @@
+#include "Parser.h"         // C_CalcParser, errors
+#include "ParserIdDef.h"    // TID_LEX_Spaces
+#include "Scanner.h"        // C_CalcScanner
+//-------------------------------------------------------------
+#include <LogStream.h>      // HRTN()
+#include <StrUtil.h>        // bux::C_IMemStream<>
+#include <iostream>         // std::cin, std::cerr
+
+enum
+{
+    MAIN_SUCCESS        =0,
+    MAIN_CAUGHT
+};
+
+int main()
+{
+    try
+    {
+        std::string line;
+        while (std::getline(std::cin, line))
+        {
+            if (line.empty())
+                // Bye!
+                break;
+
+            C_CalcParser                        parser;
+            bux::C_ScreenerNo<TID_LEX_Spaces>   preparser(parser);
+            C_CalcScanner                       scanner(preparser);
+            bux::C_IMemStream                   in{line.data(), line.size()};
+            bux::scanFile(">", in, scanner);
+
+            // Check if parsing is ok
+            if (errors)
+            {
+                errors =0;
+                std::cerr <<"Fail to parse!\n";
+                continue;
+            }
+
+            // Acceptance
+            if (!parser.accepted())
+            {
+                std::cerr <<"Incomplete expression!\n";
+                continue;
+            }
+
+            // Output the result
+            auto ans = bux::unlex<int>(parser.getFinalLex());
+            std::cerr <<"= " <<ans <<" = 0x" <<std::hex <<ans <<" = 0" <<std::oct <<ans <<std::dec <<'\n';
+        }
+
+        // Ok
+        std::cerr <<"Mission complete!\n";
+        return MAIN_SUCCESS;
+    }
+    catch (const std::exception &t)
+    {
+        std::cerr <<HRTN(t) <<": " <<t.what() <<" ... \r\n";
+        return MAIN_CAUGHT;
+    }
+    catch (...)
+    {
+        std::cerr <<"Unknown exception\r\n";
+        return MAIN_CAUGHT;
+    }
+}
